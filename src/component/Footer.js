7edff517d224bf34e7e407cbf8c436eb552654b0
Akanksha <sononeakanksha2@gmail.com>
2022-08-31 15:24:11 +0530

    updated

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
 
     return (
-        <footer className="text-light py-2" style={{ backgroundColor: "#011638", position: "scroll", width: "100%" }}>
+        <footer className="text-light py-2" style={{ backgroundColor: "#011638", position: "fixed", bottom :"0" , width: "100%" }}>
 
             <div className="footer-copyright text-center">Developed by :
                 <a href="https://www.linkedin.com/in/akanksha-sonone-0b0167211/" target="_blank" rel="noreferrer" style={{ color: "aquamarine" }} className='mx-2'>Akanksha Sonone</a>
